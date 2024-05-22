bf2201696e872594aae26f1acfee8dc12ee55650
Yago Pereira <yago.pereira@opt.com.br>
2024-05-22 10:14:59 -0300

    fix: modulo de envio de email

--- backend/src/useCases/auth/SendSecurityEmail/SendSecurityEmailUC.ts
+++ backend/src/useCases/auth/SendSecurityEmail/SendSecurityEmailUC.ts
@@ -1,48 +1,57 @@
 import IUserRepository from "../../../repositories/IUserRepository";
-import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import nodemailer from 'nodemailer';
+import * as dotenv from 'dotenv'
 
+dotenv.config();
 
 export default class SendSecurityEmailUC {
-    constructor(
-        private userRepository:IUserRepository,
-    ) {}
+  constructor(
+    private userRepository: IUserRepository,
+  ) {}
 
-    async execute() : Promise<number> {
-        const users = await this.userRepository.findAll();
-        const emails = []
+  async execute(): Promise<number> {
+    const users = await this.userRepository.findAll();
+    const emails = [];
 
-        users.forEach(element => {
-            let email = this.sendEmail(element.email)
-            if (email !== null) {
-               emails.push(email) 
-            }
-        });
-
-        if (emails.length == 0) throw new Error('Falha ao enviar emails')
-        
-        return emails.length;
+    for (const user of users) {
+      try {
+        const email = await this.sendEmail(user.email);
+        if (email !== null) {
+          console.log(`Email enviado para: ${email}`);
+          emails.push(email);
+        }
+      } catch (error) {
+        console.error(`Falha ao enviar email pra: ${user.email}`, error);
+      }
     }
 
-    sendEmail = async (email: String) => {
+    if (emails.length === 0) throw new Error('Falha ao enviar emails');
+
+    return emails.length;
+  }
+
+  sendEmail = async (email: string) => {
     try {
-      const serviceId = 'service_kqomqwj';
-      const templateId = 'template_qarf2lg';
-      const userId = 'l5AVz2asaABljn9nL';
-      const templateParams = {
-        message: 'Nosso sistema foi atacado',
-        to_email: `${email}`,
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: `${process.env.SEND_EMAIL_USER}`,
+          pass: `${process.env.SEND_EMAIL_PASS}`,
+        },
+      });
+
+      const mailOptions = {
+        from: `${process.env.SEND_EMAIL_USER}`,
+        to: email,
+        subject: 'Alerta de Segurança',
+        text: 'Nosso sistema foi atacado',
       };
 
-      const result: EmailJSResponseStatus = await emailjs.send(
-        serviceId,
-        templateId,
-        templateParams,
-        userId
-      );
-      console.log(result);
+      const result = await transporter.sendMail(mailOptions);
+      console.log(`EmailJS Response for ${email}:`, result);
       return email;
     } catch (error) {
-      console.log(error);
+      console.error(`Error sending email to ${email}:`, error);
       return null;
     }
   };
